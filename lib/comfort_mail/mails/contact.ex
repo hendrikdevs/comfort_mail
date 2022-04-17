f3b597f994a15a8e27f7d8d8e7aa59804d94742c
Hendrik <hendrik98@protonmail.com>
2022-04-18 00:10:32 +0200

    Validate email is in valid format

--- lib/comfort_mail/mails/contact.ex
+++ lib/comfort_mail/mails/contact.ex
@@ -2,6 +2,8 @@ defmodule ComfortMail.Mails.Contact do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias EctoCommons.EmailValidator
+
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "contacts" do
@@ -15,5 +17,6 @@ defmodule ComfortMail.Mails.Contact do
     contact
     |> cast(attrs, [:email])
     |> validate_required([:email])
+    |> EmailValidator.validate_email(:email)
   end
 end
