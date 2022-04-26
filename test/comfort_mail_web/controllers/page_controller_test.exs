1f6fdc06c7e9bcc2c9f3524010f51fe8bbc7526f
Hendrik <hendrik98@protonmail.com>
2022-04-26 22:59:46 +0200

    removed unused page controller and test

--- test/comfort_mail_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ComfortMailWeb.PageControllerTest do
-  use ComfortMailWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
-end
