53dda0eb796e0aa20acbdc4c81b9dbb4c73644f4
Hendrik <hendrik98@protonmail.com>
2022-04-26 20:54:30 +0200

    implemented actual welcome email

--- lib/comfort_mail/mails/contact_notifier.ex
+++ lib/comfort_mail/mails/contact_notifier.ex
@@ -1,18 +1,38 @@
 defmodule ComfortMail.Mails.ContactNotifier do
   import Swoosh.Email
+
   alias ComfortMail.Mailer
+  alias ComfortMail.Mails.Contact
 
   @company_name "ComfortMail"
   @system_mail "no-reply@comfort-mail.com"
 
-  def deliver_welcome(%{name: name, email: email}) do
+  def deliver_welcome(contact = %Contact{}) do
     new()
-    |> to({name, email})
+    |> to({"Dear User", contact.email})
     |> from({@company_name, @system_mail})
-    |> subject("Welcome to Comfort, #{name}!")
-    |> html_body("<h1>Hello, #{name}</h1>")
-    |> text_body("Hello, #{name}\n")
+    |> subject("Welcome to Comfort!")
+    |> html_body(html_body_welcome(contact.id))
+    |> text_body(text_body_welcome(contact.id))
     |> Mailer.deliver()
+  end
+
+  defp html_body_welcome(contact_id) do
+    "
+    <h1>Thank you for registering on comfort-mail.com</h1>
+    <p>To complete the registration click on the confirmation link below:</p>
+    <a href='https://comfort-mail.com/register/#{contact_id}'>https://comfort-mail.com/register/#{contact_id}</a>
+    <p>If you did not sign up using our service, you can safely ignore this email!</p>
+    "
+  end
+
+  defp text_body_welcome(contact_id) do
+    "
+    Thank you for registering on comfort-mail.com\n
+    To complete the registration click on the confirmation link below:\n
+    https://comfort-mail.com/register/#{contact_id}\n\n
+    If you did not sign up using our service, you can safely ignore this email!
+    "
   end
 
   def deliver_unregister(%{name: name, email: email}) do
