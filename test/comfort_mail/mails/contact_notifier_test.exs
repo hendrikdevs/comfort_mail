5099718dcd06eed99a816cb856f24889fb79dcfc
Hendrik <hendrik98@protonmail.com>
2022-04-26 22:53:48 +0200

    fixed deliver welcome test

--- test/comfort_mail/mails/contact_notifier_test.exs
+++ test/comfort_mail/mails/contact_notifier_test.exs
@@ -2,17 +2,21 @@ defmodule ComfortMail.Mails.ContactNotifierTest do
   use ExUnit.Case, async: true
   import Swoosh.TestAssertions
 
+  import ComfortMail.MailsFixtures
+
+  alias ComfortMail.Mails.Contact
   alias ComfortMail.Mails.ContactNotifier
 
-  test "deliver_welcome/1" do
-    user = %{name: "Alice", email: "alice@example.com"}
 
-    ContactNotifier.deliver_welcome(user)
+  test "deliver_welcome/1" do
+    contact = contact_fixture()
+
+    ContactNotifier.deliver_welcome(contact)
 
     assert_email_sent(
-      subject: "Welcome to Phoenix, Alice!",
-      to: {"Alice", "alice@example.com"},
-      text_body: ~r/Hello, Alice/
+      subject: "Welcome to Comfortmail!",
+      to: {"Dear User", contact.email},
+      text_body: ~r/#{contact.id}/
     )
   end
 
