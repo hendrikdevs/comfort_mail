d897bf4ff4af800260910ca0981ab1d357e1d723
Hendrik <hendrik98@protonmail.com>
2022-04-27 14:21:29 +0200

    use implicit try

--- lib/comfort_mail_web/controllers/registration_controller.ex
+++ lib/comfort_mail_web/controllers/registration_controller.ex
@@ -6,27 +6,24 @@ defmodule ComfortMailWeb.RegistrationController do
   alias ComfortMail.Mails.Contact
 
   def activate(conn, %{"id" => id}) do
-    try do
-      contact = Mails.get_contact!(id)
-      Logger.debug("Got contact: #{inspect(contact)}")
+    contact = Mails.get_contact!(id)
+    Logger.debug("Got contact: #{inspect(contact)}")
 
-      case Mails.activate_contact(contact) do
-        {:ok, %Contact{}} ->
-          conn
-          |> put_status(200)
-          |> render("activated.html")
+    case Mails.activate_contact(contact) do
+      {:ok, %Contact{}} ->
+        conn
+        |> put_status(200)
+        |> render("activated.html")
 
-        {:error, _changeset} ->
-          conn
-          |> put_status(400)
-          |> render("activation_failed.html")
-      end
-
-    rescue
-      _ ->
+      {:error, _changeset} ->
         conn
         |> put_status(400)
         |> render("activation_failed.html")
     end
+  rescue
+    _ ->
+      conn
+      |> put_status(400)
+      |> render("activation_failed.html")
   end
 end
