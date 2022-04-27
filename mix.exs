afb107b047d88b35058ab0e5da5fb43db83cf152
Hendrik <hendrik98@protonmail.com>
2022-04-27 17:19:44 +0200

    added sobelow

--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule ComfortMail.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:ecto_commons, "~> 0.3.3"},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:sobelow, "~> 0.8", only: [:dev, :test]},
     ]
   end
 
