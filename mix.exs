6474e36347d529d266b744bdc9b67c9b2e6c0d05
Hendrik <hendrik98@protonmail.com>
2022-04-27 13:13:55 +0200

    added credo

--- mix.exs
+++ mix.exs
@@ -49,7 +49,8 @@ defmodule ComfortMail.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:ecto_commons, "~> 0.3.3"}
+      {:ecto_commons, "~> 0.3.3"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
   end
 
