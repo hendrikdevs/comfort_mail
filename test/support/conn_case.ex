670f9136db8f8bbc7cad3a7f69650f27fdec4466
Hendrik <hendrik98@protonmail.com>
2022-05-07 21:54:56 +0200

    fixed testing

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -35,7 +35,7 @@ defmodule ComfortMailWeb.ConnCase do
 
   setup tags do
     pid = Sandbox.start_owner!(ComfortMail.Repo, shared: not tags[:async])
-    on_exit(fn -> SQL.Sandbox.stop_owner(pid) end)
+    on_exit(fn -> Sandbox.stop_owner(pid) end)
     {:ok, conn: Phoenix.ConnTest.build_conn()}
   end
 end
